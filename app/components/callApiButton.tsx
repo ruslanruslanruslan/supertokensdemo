3ec2905809c77f2375dbbd1bc3c200e52bc973c7
ruslan <ruslan.ibragimov@sravni.ru>
2024-11-09 00:03:04 +0800

    fix hardcoded appInfo

--- app/components/callApiButton.tsx
+++ app/components/callApiButton.tsx
@@ -2,9 +2,11 @@
 
 import styles from "../page.module.css";
 
+import { appInfo } from "../config/appInfo";
+
 export const CallAPIButton = () => {
     const fetchUserData = async () => {
-        const userInfoResponse = await fetch("http://localhost:3000/api/user");
+        const userInfoResponse = await fetch(`${appInfo.apiDomain}/api/user`);
 
         alert(JSON.stringify(await userInfoResponse.json()));
     };
