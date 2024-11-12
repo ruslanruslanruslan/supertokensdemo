47425e1faf6882701a304bcfa92f2295efce50e2
ruslan <ruslan.ibragimov@sravni.ru>
2024-11-12 11:44:42 +0800

    correct domain and call api bugfix

--- app/config/appInfo.ts
+++ app/config/appInfo.ts
@@ -1,7 +1,7 @@
 export const appInfo = {
     appName: "SuperTokens Next.js demo app",
-    apiDomain: "http://192.168.2.129:30019",
-    websiteDomain: "http://localhost:3000",
+    apiDomain: "http://kube.ibragimov.pw:30019",
+    websiteDomain: "http://local.ibragimov.pw:3000",
     apiBasePath: "/api/auth",
     websiteBasePath: "/auth",
 };
