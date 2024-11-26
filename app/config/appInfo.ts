d0030167ab3ec6a32137219846bb36108bb3190f
ruslan <ruslan.ibragimov@sravni.ru>
2024-11-26 23:54:06 +0700

    https urls

--- app/config/appInfo.ts
+++ app/config/appInfo.ts
@@ -1,7 +1,7 @@
 export const appInfo = {
     appName: "SuperTokens Next.js demo app",
-    apiDomain: "http://kube.ibragimov.pw:30019",
-    websiteDomain: "http://local.ibragimov.pw:3000",
+    apiDomain: "https://master.docway.ibragimov.pw",
+    websiteDomain: "https://local.ibragimov.pw:3000",
     apiBasePath: "/api/auth",
     websiteBasePath: "/auth",
 };
